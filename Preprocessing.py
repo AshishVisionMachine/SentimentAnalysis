d7ba034ef0e8d1b58a6a8c53f4da4fc1aa6ee48d
AshishVisionMachine <84072143+AshishVisionMachine@users.noreply.github.com>
2021-05-18 10:07:08 +0530

    Update Preprocessing.py
    Function addition for text peprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,33 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 class preprocessing:
 
     def __init__(self,input):
         self.input=input
+        
+    def lowercasing(self,input):
+        lower_string=input.lower()
+        
+        retrun lower_string
+        
+    def tokenization(self,input):
+        token_string=word_tokenize(input)
+        
+        retrun token_string
+        
+    def stopwordemoval(self,input):
+        stop_words=set(stopwords.words('english'))
+        
+        retrun stop_words
+        
+    def stemming(self,input):
+        ps = PorterStemmer()
+        stem_word=ps.stem(input)
+        retrun stem_word
+        
+    def lemmatization(self,input):
+        
+        retrun lemmatization_string
